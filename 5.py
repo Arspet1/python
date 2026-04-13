a4ab186f99ea8d61550bac3e1e9d50f05e806090
Arspet1 <fieriatania@gmail.com>
2026-04-13 23:59:43 +0300

    Delete 5.py

--- 5.py
+++
@@ -1,8 +0,0 @@
-with open("report.txt", "r") as f:
-    content = f.read()
-content = content.replace("old", "new")
-
-with open("report.txt", "w") as f:
-    f.write(content)
-
-print(content)            
